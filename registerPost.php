a1eaff18e5144868163149cdefbcca5847406b4f
bau- <last.ironman@gmail.com>
2011-03-08 00:25:11 +0100

    Viene mandata la mail all'utente per convalidare l'account, ecc. Ancora incompleto: manca da controllare che la mail non sia già registrata nel database

--- registerPost.php
+++ registerPost.php
@@ -28,34 +28,58 @@ if ($_POST['submit']) {
 	$password=$_POST['password'];
 	$confirm=$_POST['passwordC'];
 	$mail=$_POST['mail'];
+	$mail2=$_POST['mail2'];
+	
+	$errore=0;
+	// Verifica se le due mail sono uguali
+	if ($mail!=$mail2)
+	{
+		echo "Errore! I due indirizzi e-mail non coincidono!";
+		$errore=1;
+	}
+	
+	if (!$mail&&!$mail2)
+	{
+		echo "Errore! Devi inserire l'indirizzo e-mail!";
+		$errore=1;
+	}
 
 	if  ($password != $confirm)
-	/* verifica se le password e la conferma sono uguali */
-	echo "Errore! Password e Conferma sono diverse. ";
+	{
+		/* verifica se le password e la conferma sono uguali */
+		echo "Errore! Le due Password non coincidono!";
+		$errore=1;
+	}
+	
 	elseif (get_pwd($login))
-	echo "Errore! Login gia` in uso. ";
-	else {
-	/* inserisce il login e password nella BD */
+	{
+		echo "Errore! Login gia` in uso. ";
+		$errore=1;
+	}
+	
+	if (!$errore)	// Se NON ci sono stati errori:
+	{
+		/* inserisce il login e password nella BD */
+		/* connessione al server */	
+	
+		$conn=mysql_connect(dbhost, dbuser, dbpwd)
+		or die("Connessione al server MySQL fallita!");
 
-
-	/* connessione al server */
-	$conn=mysql_connect(dbhost, dbuser, dbpwd)
-	or die("Connessione al server MySQL fallita!");
-
-	mysql_select_db(dbname);
-  
-	$query="SELECT * FROM Stato";
-	$result=mysql_query($query, $conn)
-	  or die("Query fallita!" . mysql_error());
-	$stato=mysql_fetch_array($result);
-	$query= "INSERT INTO Utenti (username,password,voto) VALUES ('$login', '".SHA1($password)."', '".$voto['Round']."')";
-
-	mysql_query($query,$conn)
-		or die("Query fallita" . mysql_error($conn));
-
- }
+		mysql_select_db(dbname);
+  		$code = rand(1, 999);	// Genera un numero casuale
+		echo $code;
+		$query="SELECT * FROM Stato";
+		$result=mysql_query($query, $conn)
+		  or die("Query fallita!" . mysql_error());
+		$stato=mysql_fetch_array($result);
+		$query= "INSERT INTO Utenti (username,password,voto,mail,verificato,temp) VALUES ('$login', '".SHA1($password)."', '".$voto['Round']."', '$mail', '0', '$code')";
+		echo $code;
+		mysql_query($query,$conn)
+			or die("Query fallita" . mysql_error($conn));
+		mail ("$mail", "Conferma Indirizzo Project Lumière", "Clicca questo link per validare il tuo account! http://monossido.ath.cx/bau/cinema/verifica.php?code=$code&nome=$login", "From: Project Lumière");
+		echo "Registrazione Effettuata! Devi validare il tuo account! Per farlo, clicca il link che trovi nella mail che &egrave; stata mandata all'indirizzo che ci hai fornito.";
+ 	}
 }
-else echo "Registrazione Effettuata";
 
 ?>
 
