cbc51f601ac91aa88ad6e95cdbfee073288e637f
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2011-03-08 20:53:53 +0100

    loloas dimenticanza sorry :)

--- configure.php
+++ configure.php
@@ -3,7 +3,7 @@
   /* parametri per la connessione */
   define('dbhost','localhost');     /* server MySQL */
   define('dbuser','cinema');              /* utente       */
-  define('dbpwd','cinema');               /* password     */
+  define('dbpwd','proiettore');               /* password     */
   define('dbname','cinema');
 
 ?>
