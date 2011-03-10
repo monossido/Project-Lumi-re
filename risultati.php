4523c2a876ae1eee471e4cfe5f7bdfdd8a4ecac5
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2011-03-10 12:54:55 +0100

    BugFix Live, sistemazione if, il grafico non viene fisualizzato da chi non ha votato per il round in corso

--- risultati.php
+++ risultati.php
@@ -31,29 +31,34 @@ $result=mysql_query($query, $conn)
   or die("Query fallita!" . mysql_error());
 $stato=mysql_fetch_array($result);
 
-if($stato['VotazioniAperte'] && $stato['Round'])
+if($stato['VotazioniAperte'] || $stato['Round']>0)//Devo far vedere i risultati solo quando si è iniziata una votazione cioè round>0 oppure VotazioniAperte=1 e round=0
 {
+	$query5="SELECT Voto FROM Utenti WHERE Username='".$_SESSION['logged']."'";
+	$result5=mysql_query($query5, $conn)
+	  or die("Query fallita!" . mysql_error());
+	$voto=mysql_fetch_array($result5);
+	if($voto['Voto']>=$stato['Round'])//Solo se l'utente ha già votato per quel round faccio vedere i risultati parziali
+	{
+		# Grafico
+		$chart = new HorizontalBarChart(1100, 300);
+		$dataSet = new XYDataSet();
 
-	# Grafico
+		$query="SELECT titolo,voti FROM Film WHERE voti>0";
+		$result=mysql_query($query, $conn)
+		 or die("Query fallita!" . mysql_error());
 
-	    $chart = new HorizontalBarChart(1100, 300);
-	    $dataSet = new XYDataSet();
+		while($row=mysql_fetch_array($result))
+		{
+		$dataSet->addPoint(new Point($row['titolo'], $row['voti']));
+		}
 
-	    $query="SELECT titolo,voti FROM Film WHERE voti>0";
-	    $result=mysql_query($query, $conn)
-	 	 or die("Query fallita!" . mysql_error());
+		$chart->setDataSet($dataSet);
+		$chart->getPlot()->setGraphPadding(new Padding(10, 10, 10, 200));
+		$chart->setTitle("Risultati parziali film");
+		$chart->render("grafici/grafico1.png");
 
-	    while($row=mysql_fetch_array($result))
-	    {
-	    	$dataSet->addPoint(new Point($row['titolo'], $row['voti']));
-	    }
-
-	    $chart->setDataSet($dataSet);
-	    $chart->getPlot()->setGraphPadding(new Padding(10, 10, 10, 200));
-	    $chart->setTitle("Risultati parziali film");
-	    $chart->render("grafici/grafico1.png");
-
-	    echo '<p  align=center><img src="grafici/grafico1.png" alt="grafico" /></p>';
+		echo '<p  align=center><img src="grafici/grafico1.png" alt="grafico" /></p>';
+	}
 
 	if($stato['Round']==1 && $stato['VotazioniAperte']==1)
 		echo "<br /><p align=center><b>Film passati al secondo turno:</b></p>";
@@ -63,18 +68,14 @@ if($stato['VotazioniAperte'] && $stato['Round'])
 	{
 		# Stampa tabella film votati
 			# Riempio Tabella
-			$query="SELECT * FROM Film WHERE passato=1";
+			$query="SELECT * FROM Film WHERE passato=".$stato['Round']."";
 			$result=mysql_query($query, $conn)
 			  or die("Query fallita!" . mysql_error());
 
 			echo "<TABLE width=\"55%\" border=1 align=center cellpadding=5><th>Titolo</th><th>Risoluzione</th><th>Lingua</th><th>Durata</th><th>Vota</th>";
-			while($film=mysql_fetch_array($result4))
+			while($film=mysql_fetch_array($result))
 			{
 				echo "<form action=film.php method=get><tr><td><a href=film.php?titolo=".$film['titolo'].">".$film['titolo']."</a></td><td>".$film['risoluzione']."</td><td>".$film['lingua']."</td><td>".$film['durata']."</td>";
-				$query5="SELECT Voto FROM Utenti WHERE Username='".$_SESSION['logged']."'";
-				$result5=mysql_query($query5, $conn)
-				  or die("Query fallita!" . mysql_error());
-				$voto=mysql_fetch_array($result5);
 				if(isset($_SESSION['logged']) && $voto['Voto']=="1")
 					echo "<td><a href='vota.php?id=".$film['id_film']."&round=".$stato['Round']."'>Vota</a></td>";
 				else echo "<td width='8%'><em>Non puoi votare</em></td>";
