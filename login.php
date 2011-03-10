aba24a4b240ca4766491bece7b0348f1026e8920
bau- <last.ironman@gmail.com>
2011-03-11 00:11:00 +0100

    Viene visualizzato nella pagina se l'utente inserisce username e password non corretti.

--- login.php
+++ login.php
@@ -20,16 +20,47 @@ require("library.php");
 session_start();
 page_start("Project Lumi&eacute;re - loggati");
 
+$conn=mysql_connect(dbhost, dbuser, dbpwd)
+	or die('Connessione al server MySQL fallita!');
+mysql_select_db(dbname);
+
 if(isset($_SESSION['logged']))
 	echo "Devi prima fare il logout";
 else
 {
-echo "<center>";
-echo "<table border=0>";
-	echo 	"<form method=POST action=index.php>
-	<tr><td>Username:</td><td><input type=text name=username></td><tr />
-	<tr><td>Password:</td><td><input type=password name=password></td><tr /></table><br>
-	<input type=submit name=login value=login></form>";
+	if (!$_POST['login'])
+	{	
+		echo "<center>";
+		echo "<table border=0>";
+		echo 	"<form method=POST action=login.php>
+		<tr><td>Username:</td><td><input type=text name=username></td><tr />
+		<tr><td>Password:</td><td><input type=password name=password></td><tr /></table><br>
+		<input type=submit name=login value=login></form>";
+	}
+	else
+	{
+		$login=$_POST['username']; 
+		$password=$_POST['password'];
+		$query="SELECT * FROM Utenti WHERE Username='$login'";
+		$result=mysql_query($query, $conn)
+		  or die('Query fallita!' . mysql_error());
+		$stato=mysql_fetch_array($result);				
+		$password2=$stato['Password'];
+
+		if (!$password2)	// Se vuoto vuol dire che la query non è andata -> username errato
+			echo "Username errato! <a href=login.php>Riprova</a>.";
+		else
+		{
+			echo $stato['username'];
+			if (SHA1($password) != get_pwd($login))
+				echo "<br>Errore! Password Errata! <a href=login.php>Riprova</a>";
+			else
+			{
+				echo "Login Effettuato! Torna alla <a href=index.php>Home</a>";
+				$_SESSION['logged']=$login;
+			}
+		}
+	}
 }
 
 ?>
