9b9c288a76924881187fb81445f0d679855c52d5
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2011-03-22 22:51:56 +0100

    Accorpato registerPost.php a register.php

--- register.php
+++ register.php
@@ -40,26 +40,84 @@ $result=mysql_query($query, $conn)
   or die("Query fallita!" . mysql_error());
 $stato=mysql_fetch_array($result);
 
-echo "<p align='center'>";
-if($_SESSION['logged'])
+if (!$_POST['submit'])
 {
-	echo "Sei già loggato, effettua il <a href='logout.php'>logout</a> prima.";
-}
-else if(!$stato['RegistrazioniAperte'])
-{
-	echo "Le registrazioni sono chiuse";
-}else
-{
-	echo "<table border=0>";
+	echo "<p align='center'>";
+	if($_SESSION['logged'])
+	{
+		echo "Sei già loggato, effettua il <a href='logout.php'>logout</a> prima.";
+	}
+	else if(!$stato['RegistrazioniAperte'])
+	{
+		echo "Le registrazioni sono chiuse";
+	}else
+	{
+		echo "<table border=0>";
 
-	echo 	"<form method=POST action=registerPost.php>
-		<tr><td>Username:</td><td><input type=text name=username></td><tr />
-		<tr><td>Password:</td><td><input type=password name=password></td><tr />
-		<tr><td>Conferma Password:</td><td><input type=password name=passwordC></td><tr />
-		<tr><td>E-mail:</td><td><input type=text name=mail></td><tr />
-		<tr><td>Conferma E-mail:</td><td><input type=text name=mail2 autocomplete=off></td><tr /></table><br>
-		<input type=submit name=submit value=registrati></form>";
+		echo 	"<form method=POST action=register.php>
+			<tr><td>Username:</td><td><input type=text name=username></td><tr />
+			<tr><td>Password:</td><td><input type=password name=password></td><tr />
+			<tr><td>Conferma Password:</td><td><input type=password name=passwordC></td><tr />
+			<tr><td>E-mail:</td><td><input type=text name=mail></td><tr />
+			<tr><td>Conferma E-mail:</td><td><input type=text name=mail2 autocomplete=off></td><tr /></table><br>
+			<input type=submit name=submit value=registrati></form>";
+	}
+	echo "</p>";
+}else{
+	/* recupera i dati immessi */
+	$login=$_POST['username'];
+	$password=$_POST['password'];
+	$confirm=$_POST['passwordC'];
+	$mail=$_POST['mail'];
+	$mail2=$_POST['mail2'];
+	
+	$errore=0;
+	// Verifica se le due mail sono uguali
+	if ($mail!=$mail2)
+	{
+		echo "Errore! I due indirizzi e-mail non coincidono!";
+		$errore=1;
+	}
+	
+	if (!$mail&&!$mail2)
+	{
+		echo "Errore! Devi inserire l'indirizzo e-mail!";
+		$errore=1;
+	}
+
+	if  ($password != $confirm)
+	{
+		/* verifica se le password e la conferma sono uguali */
+		echo "Errore! Le due Password non coincidono!";
+		$errore=1;
+	}
+	
+	elseif (get_pwd($login))
+	{
+		echo "Errore! Login gia` in uso. ";
+		$errore=1;
+	}
+	
+	if (!$errore)	// Se NON ci sono stati errori:
+	{
+		/* inserisce il login e password nella BD */
+		/* connessione al server */	
+	
+		$conn=mysql_connect(dbhost, dbuser, dbpwd)
+		or die("Connessione al server MySQL fallita!");
+
+		mysql_select_db(dbname);
+  		$code = rand(1, 9999);	// Genera un numero casuale
+		$query="SELECT * FROM Stato";
+		$result=mysql_query($query, $conn)
+		  or die("Query fallita!" . mysql_error());
+		$stato=mysql_fetch_array($result);
+		$query= "INSERT INTO Utenti (username,password,voto,mail,verificato,temp) VALUES ('$login', '".SHA1($password)."', '".$voto['Round']."', '$mail', '0', '$code')";
+		mysql_query($query,$conn)
+			or die("Query fallita" . mysql_error($conn));
+		mail ("$mail", "Conferma Indirizzo Project Lumière", "Clicca questo link per validare il tuo account! http://monossido.ath.cx/bau/cinema/verifica.php?code=$code&nome=$login", "From: Project_Lumière");
+		echo "Registrazione Effettuata! Devi validare il tuo account! Per farlo, clicca il link che trovi nella mail che &egrave; stata mandata all'indirizzo che ci hai fornito.";
+ 	}
 }
-echo "</p>";
 
 ?>
